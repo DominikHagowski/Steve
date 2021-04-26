f2719315662afdf4c8cc41117b8dc6511208e15b
DominikHagowski <DominikHagowski@gmail.com>
2021-04-26 17:09:44 +0100

    Added better sprite and background rendering
    Instead of clearing the screen every frame the background is drawn once and only the changes between frames are rendered again

--- Program.cs
+++ Program.cs
@@ -23,6 +23,22 @@ namespace Steve
             }
         }
 
+        public struct Quad
+        {
+            public double X;
+            public double Y;
+            public double W;
+            public double H;
+
+            public Quad(int inx, int iny, int inh, int inw)
+            {
+                X = inx;
+                Y = iny;
+                W = inw;
+                H = inh;
+            }
+        }
+
         namespace Console
         {
             public struct ConsoleCharacter //The character type of each character on the screen
@@ -39,12 +55,15 @@ namespace Steve
                 public ConsoleColor BackGround; //Background colour
                 public ConsoleColor ForeGround; //Foreground colour
                 public Vector2 Position; //Position on the screen
+                public Vector2 OldPosition; //The previous position on the screen
+                public Vector2 Size; //The size of the sprite
                 public bool Update; //If the sprite should be updated
                 
                 //Changes position by a certain vector2
                 public void ChangePosition(Vector2 Difference) 
                 {
                     Update = true; //redraw the sprite
+                    OldPosition = Position;
                     Position.X += Difference.X;
                     Position.Y += Difference.Y;
                 }
@@ -53,6 +72,7 @@ namespace Steve
                 public void SetPosition(Vector2 NewPosition)
                 {
                     Update = true; //redraw the sprite
+                    OldPosition = Position;
                     Position = NewPosition;
                 }
 
@@ -77,88 +97,97 @@ namespace Steve
                 Vector2 ScreenSize;
 
                 //Should the entire screen be redrawn
-                public bool RedrawScreen = false;
+                public bool RedrawScreen = true;
 
-                //called every frame to draw the screen
-                public void Draw()
+                public void DrawArea(Quad Area)
                 {
-                    //clear console
-                    System.Console.Clear();
 
                     //clear colours to default
                     System.Console.BackgroundColor = ConsoleColor.Black;
                     System.Console.ForegroundColor = ConsoleColor.White;
 
                     //for each character in the screen
-                    for (int y = 0; y < ScreenSize.Y - 1; y++)
+                    for (int y = (int)Area.Y; y < ((int)Area.Y + (int)Area.H) - 1; y++)
                     {
-                        for (int x = 0; x < ScreenSize.X - 1; x++)
+                        for (int x = (int)Area.X; x < ((int)Area.X + (int)Area.W) - 1; x++)
                         {
                             //clear colours to default
                             System.Console.BackgroundColor = ConsoleColor.Black;
                             System.Console.ForegroundColor = ConsoleColor.White;
-
-                            //draw every character that needs to be redrawn
-                            if (Screen[x, y].Update == true || RedrawScreen == true)
+                            try
                             {
                                 //Set cursor to position
                                 System.Console.SetCursorPosition(x, y);
-                                
+
                                 //set colour to character colour
                                 System.Console.BackgroundColor = Screen[x, y].Background;
                                 System.Console.ForegroundColor = Screen[x, y].Forground;
-                                
+
                                 //write character
                                 System.Console.Write(Screen[x, y].Character);
-                                
-                                //doesnt need to be redrawn next frame until a change is made
-                                Screen[x, y].Update = false;
-                            }
-                        }
 
-                        //go through each sprite
-                        foreach (Sprite S in Sprites)
-                        {
-                            //and draw the ones that need updating
-                            if (S.Update == false || RedrawScreen == true)
+                                 //doesnt need to be redrawn next frame until a change is made
+                                 Screen[x, y].Update = false;
+                            } catch
                             {
-                                //Would break if written outside the bounds, hence the try catch block. 
-                                //TODO: remove this in the future
-                                try
-                                {
-                                    //set position to the sprite
-                                    System.Console.SetCursorPosition((int)S.Position.X, (int)S.Position.Y);
-                                    
-                                    //set colour
-                                    System.Console.BackgroundColor = S.BackGround;
-                                    System.Console.ForegroundColor = S.ForeGround;
-
-                                    //split sprite into lines
-                                    String[] Lines = S.Graphic.Split('\n');
-
-                                    //Draw each line of the sprite
-                                    for (int i = 0; i < Lines.Length; i++)
-                                    {
-                                        System.Console.SetCursorPosition((int)S.Position.X, (int)S.Position.Y + i);
-                                        System.Console.Write(Lines[i]);
-                                    }
-                                }
-                                catch
-                                {
-                                    break;
-                                }
-                                //dont need to update anymore
-                                S.Update = true;
+                                break;
                             }
                         }
-                        //reset cursor
-                        System.Console.SetCursorPosition(0, 0);
-                        //dont need to redraw screen
-                        RedrawScreen = false;
                     }
-                    
-                    
+                }
 
+                //called every frame to draw the screen
+                public void Draw()
+                {
+
+                    if (RedrawScreen == true)
+                    {
+                        DrawArea(new Quad(0, 0, (int)ScreenSize.Y, (int)ScreenSize.X));
+                    }
+
+                    //go through each sprite
+                    foreach (Sprite S in Sprites)
+                    {
+                        if(S.Update == true)
+                        {
+                            DrawArea(new Quad((int)S.OldPosition.X, (int)S.OldPosition.Y, (int)S.Size.Y, (int)S.Size.X));
+                        }
+                        //and draw the ones that need updating
+                        if (true)
+                        {
+                            //Would break if written outside the bounds, hence the try catch block. 
+                            //TODO: remove this in the future
+                            try
+                            {
+                                //set position to the sprite
+                                System.Console.SetCursorPosition((int)S.Position.X, (int)S.Position.Y);
+                                    
+                                //set colour
+                                System.Console.BackgroundColor = S.BackGround;
+                                System.Console.ForegroundColor = S.ForeGround;
+
+                                //split sprite into lines
+                                String[] Lines = S.Graphic.Split('\n');
+
+                                //Draw each line of the sprite
+                                for (int i = 0; i < Lines.Length; i++)
+                                {
+                                    System.Console.SetCursorPosition((int)S.Position.X, (int)S.Position.Y + i);
+                                    System.Console.Write(Lines[i]);
+                                }
+                            }
+                            catch
+                            {
+                                break;
+                            }
+                            //dont need to update anymore
+                            S.Update = true;
+                        }
+                    }
+                    //reset cursor
+                    System.Console.SetCursorPosition(0, 0);
+                    //dont need to redraw screen
+                    RedrawScreen = false;
                 }
 
                 public ConsoleWriter(Vector2 Size)
@@ -191,7 +220,7 @@ namespace Steve
         static void Main(string[] args)
         {
             Graphics.Vector2 Size;
-            Size.X = 60;
+            Size.X = 90;//60;
             Size.Y = 30;
 
 
@@ -202,9 +231,15 @@ namespace Steve
                 ConsoleOutput.Screen[12, i].Character = (char)(i + 97);
             }
 
-            Graphics.Console.Sprite h = new Graphics.Console.Sprite("This\nIs\nA\nMulti-Line\nSprite", new Graphics.Vector2(10, 20),ConsoleColor.Black, ConsoleColor.Cyan);
+            for (int i = 0; i < 90; i++)
+            {
+                ConsoleOutput.Screen[i, 15].Character = (char)((i % 20) + 97);
+            }
 
-            ConsoleOutput.Sprites.Add(h);//
+            Graphics.Console.Sprite h = new Graphics.Console.Sprite("This\nIs\nA\nMulti-Line\n        Sprite", new Graphics.Vector2(10, 20),ConsoleColor.Black, ConsoleColor.Cyan);
+            h.Size = new Graphics.Vector2(15, 10);
+
+            ConsoleOutput.Sprites.Add(h);
             ConsoleOutput.Screen[0, 10].Character = 'O';
             ConsoleOutput.Screen[0, 10].Background = ConsoleColor.Red;
             ConsoleOutput.Screen[0, 10].Forground = ConsoleColor.Yellow;
@@ -213,11 +248,12 @@ namespace Steve
                 ConsoleOutput.Draw();
                 h.Update = false;
                 ConsoleOutput.Screen[0, 10].Update = true;
-                ConsoleOutput.RedrawScreen = true;
+                h.ChangePosition(new Graphics.Vector2(1, -1));
                 for (int i = 0; i < 10; i++)
                 {
                     ConsoleOutput.Screen[12, i].Update = true;
                 }
+                System.Threading.Thread.Sleep(1);
             }
         }
     }
